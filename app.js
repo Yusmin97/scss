399f6f246ed14dfd5fbd19c7dac2756bae2da691
YuSeungMin <tmdamsi97@gmail.com>
2023-10-10 13:08:54 +0900

    포켓몬 한글로 변환 후 html파일 생성기 생성

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const pokemon = require('pokemon');
 
+const pokemonT = pokemon.all('ko');
+// const pokeArray = [];
+
+// for (let i = 0; i < pokemonT.length; i++) {
+//   pokeArray[i] = pokemonT[i];
+// }
+// console.log(pokeArray[i]);
+
+const pokeJOin = pokemonT.join();
+
+console.log(pokeJOin);
+
+const fs = require('fs');
+
+fs.writeFileSync(
+  'pokemon.html',
+  `
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div#root>
+      <li>${pokeJOin}</li>
+    </div>
+  </body>
+</html>`
+);
+
+console.log(fs);
